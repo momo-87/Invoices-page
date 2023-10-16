783108a75067e82095272ba2fb7a3bf0719d0095
Momo-87 <momochristian87@gmail.com>
2023-10-16 13:19:15 +0100

    Implement the Popup window

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-ligth-blue": "#EAF2FD",
+        "color-gray-text": "#9D9D9D",
+        "color-gray-line": "#E9E9E9",
+        "color-light-red": "#FAFAFA",
+        "color-text-blue": "#4070F3"
+    }
+    },
   },
   plugins: [],
 }
